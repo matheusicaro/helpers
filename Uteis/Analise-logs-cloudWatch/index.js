7592301318683a9ac38913be09842d183d31a057
matheus <matheusicaro2@hotmail.com>
2019-11-13 17:16:47 -0300

    feature: Organize AWS Cloud Watch on the terminal.

---
+++ Uteis/Analise-logs-cloudWatch/index.js
@@ -0,0 +1,54 @@
+const LOGS_COLAR_AQUI = [
+  {
+    "message": "{\"timestamp\":\"2019-11-13 17:08:31\",\"service\":\"inter-chatbot-dti-brokers-service\",\"thread\":\"[qtp1878169648-17733]\",\"level\":\"INFO\",\"meta\":\"\\\"{\\\\\\\"type\\\\\\\":\\\\\\\"notification_event\\\\\\\",\\\\\\\"app_id\\\\\\\":\\\\\\\"qs976j62\\\\\\\",\\\\\\\"data\\\\\\\":{\\\\\\\"type\\\\\\\":\\\\\\\"notification_event_data\\\\\\\",\\\\\\\"item\\\\\\\":{\\\\\\\"type\\\\\\\":\\\\\\\"conversation\\\\\\\",\\\\\\\"id\\\\\\\":\\\\\\\"24526524935\\\\\\\",\\\\\\\"created_at\\\\\\\":1573662763,\\\\\\\"updated_at\\\\\\\":1573675701,\\\\\\\"user\\\\\\\":{\\\\\\\"type\\\\\\\":\\\\\\\"user\\\\\\\",\\\\\\\"id\\\\\\\":\\\\\\\"5d9e1b486b023fc6b2505b12\\\\\\\",\\\\\\\"user_id\\\\\\\":\\\\\\\"83034030010\\\\\\\",\\\\\\\"name\\\\\\\":\\\\\\\"RAFAEL DA SILVA CARDOSO\\\\\\\",\\\\\\\"email\\\\\\\":\\\\\\\"designeretoldos@hotmail.com\\\\\\\",\\\\\\\"do_not_track\\\\\\\":null},\\\\\\\"assignee\\\\\\\":{\\\\\\\"type\\\\\\\":\\\\\\\"admin\\\\\\\",\\\\\\\"id\\\\\\\":\\\\\\\"2646305\\\\\\\",\\\\\\\"name\\\\\\\":\\\\\\\"Babi\\\\\\\",\\\\\\\"email\\\\\\\":\\\\\\\"chatbot@bancointer.com.br\\\\\\\"},\\\\\\\"conversation_message\\\\\\\":{\\\\\\\"type\\\\\\\":\\\\\\\"conversation_message\\\\\\\",\\\\\\\"id\\\\\\\":\\\\\\\"446890396\\\\\\\",\\\\\\\"url\\\\\\\":null,\\\\\\\"subject\\\\\\\":\\\\\\\"\\\\\\\",\\\\\\\"body\\\\\\\":\\\\\\\"<p>Ola gostaria de saber como faço para ter p cartão de crédito</p>\\\\\\\",\\\\\\\"author\\\\\\\":{\\\\\\\"type\\\\\\\":\\\\\\\"user\\\\\\\",\\\\\\\"id\\\\\\\":\\\\\\\"5d9e1b486b023fc6b2505b12\\\\\\\"},\\\\\\\"attachments\\\\\\\":[]},\\\\\\\"conversation_parts\\\\\\\":{\\\\\\\"type\\\\\\\":\\\\\\\"conversation_part.list\\\\\\\",\\\\\\\"conversation_parts\\\\\\\":[{\\\\\\\"type\\\\\\\":\\\\\\\"conversation_part\\\\\\\",\\\\\\\"id\\\\\\\":\\\\\\\"4116607296\\\\\\\",\\\\\\\"part_type\\\\\\\":\\\\\\\"comment\\\\\\\",\\\\\\\"body\\\\\\\":\\\\\\\"<p>Ola</p>\\\\\\\",\\\\\\\"created_at\\\\\\\":1573675701,\\\\\\\"updated_at\\\\\\\":1573675701,\\\\\\\"notified_at\\\\\\\":1573675701,\\\\\\\"assigned_to\\\\\\\":null,\\\\\\\"author\\\\\\\":{\\\\\\\"type\\\\\\\":\\\\\\\"user\\\\\\\",\\\\\\\"id\\\\\\\":\\\\\\\"5d9e1b486b023fc6b2505b12\\\\\\\",\\\\\\\"name\\\\\\\":\\\\\\\"RAFAEL DA SILVA CARDOSO\\\\\\\",\\\\\\\"email\\\\\\\":\\\\\\\"designeretoldos@hotmail.com\\\\\\\"},\\\\\\\"attachments\\\\\\\":[],\\\\\\\"external_id\\\\\\\":null}],\\\\\\\"total_count\\\\\\\":1},\\\\\\\"conversation_rating\\\\\\\":{\\\\\\\"rating\\\\\\\":null,\\\\\\\"remark\\\\\\\":null,\\\\\\\"created_at\\\\\\\":1573670191,\\\\\\\"customer\\\\\\\":{\\\\\\\"type\\\\\\\":\\\\\\\"user\\\\\\\",\\\\\\\"id\\\\\\\":\\\\\\\"83034030010\\\\\\\"},\\\\\\\"teammate\\\\\\\":{\\\\\\\"type\\\\\\\":\\\\\\\"admin\\\\\\\",\\\\\\\"id\\\\\\\":\\\\\\\"2646305\\\\\\\"}},\\\\\\\"open\\\\\\\":true,\\\\\\\"state\\\\\\\":\\\\\\\"open\\\\\\\",\\\\\\\"snoozed_until\\\\\\\":null,\\\\\\\"read\\\\\\\":true,\\\\\\\"metadata\\\\\\\":{},\\\\\\\"tags\\\\\\\":{\\\\\\\"type\\\\\\\":\\\\\\\"tag.list\\\\\\\",\\\\\\\"tags\\\\\\\":[]},\\\\\\\"tags_added\\\\\\\":{\\\\\\\"type\\\\\\\":\\\\\\\"tag.list\\\\\\\",\\\\\\\"tags\\\\\\\":[]},\\\\\\\"links\\\\\\\":{\\\\\\\"conversation_web\\\\\\\":\\\\\\\"https://app.intercom.io/a/apps/qs976j62/conversations/24526524935\\\\\\\"}}},\\\\\\\"links\\\\\\\":{},\\\\\\\"id\\\\\\\":\\\\\\\"notif_f8a817a3-097d-4769-b095-e7bfe0afaf63\\\\\\\",\\\\\\\"topic\\\\\\\":\\\\\\\"conversation.user.replied\\\\\\\",\\\\\\\"delivery_status\\\\\\\":\\\\\\\"pending\\\\\\\",\\\\\\\"delivery_attempts\\\\\\\":1,\\\\\\\"delivered_at\\\\\\\":0,\\\\\\\"first_sent_at\\\\\\\":1573675703,\\\\\\\"created_at\\\\\\\":1573675702,\\\\\\\"self\\\\\\\":null}\\\"\"}"
+},
+{
+    "message": "{\"timestamp\":\"2019-11-13 17:08:31\",\"service\":\"inter-chatbot-dti-brokers-service\",\"thread\":\"[qtp1878169648-17733]\",\"level\":\"INFO\",\"meta\":\"ID_CONVERSA: 24526524935 - MENSAGEM: iniciandoFlow: ValidaCabecalhoIntercomFlowItem - TIMESTAMP: 2019-11-13 17:08:31.578\"}"
+},
+{
+    "message": "{\"timestamp\":\"2019-11-13 17:08:31\",\"service\":\"inter-chatbot-dti-brokers-service\",\"thread\":\"[qtp1878169648-17733]\",\"level\":\"INFO\",\"meta\":\"ID_CONVERSA: 24526524935 - MENSAGEM: finalizandoFlow: ValidaCabecalhoIntercomFlowItem - TIMESTAMP: 2019-11-13 17:08:31.579\"}"
+},
+{
+    "message": "{\"timestamp\":\"2019-11-13 17:08:31\",\"service\":\"inter-chatbot-dti-brokers-service\",\"thread\":\"[qtp1878169648-17733]\",\"level\":\"INFO\",\"meta\":\"ID_CONVERSA: 24526524935 - MENSAGEM: iniciandoFlow: VerificaChatBotHabilitadoFlowItem - TIMESTAMP: 2019-11-13 17:08:31.579\"}"
+},
+{
+    "message": "{\"timestamp\":\"2019-11-13 17:08:32\",\"service\":\"inter-chatbot-dti-brokers-service\",\"thread\":\"[qtp1878169648-17733]\",\"level\":\"INFO\",\"meta\":\"ID_CONVERSA: 24526524935 - MENSAGEM: Conversa buscada no intercom - TIMESTAMP: 2019-11-13 17:08:32.312\"}"
+},
+{
+    "message": "{\"timestamp\":\"2019-11-13 17:08:32\",\"service\":\"inter-chatbot-dti-brokers-service\",\"thread\":\"[qtp1878169648-17733]\",\"level\":\"INFO\",\"meta\":\"ID_CONVERSA: 24526524935 - MENSAGEM: MensagemAtribuidaAoUserID: 2646305; IdBabiIntercom: 2646305 - TIMESTAMP: 2019-11-13 17:08:32.312\"}"
+},
+{
+    "message": "{\"timestamp\":\"2019-11-13 17:08:32\",\"service\":\"inter-chatbot-dti-brokers-service\",\"thread\":\"[qtp1878169648-17733]\",\"level\":\"INFO\",\"meta\":\"ID_CONVERSA: 24526524935 - MENSAGEM: return mensagemDeveSerProcessada: true=> ehPrimeiraMensagem:false; estaSemAtribuicaoOuBabi:true; primeiraEntregaWebHook:true; tentativas entrega: 1 - TIMESTAMP: 2019-11-13 17:08:32.312\"}"
+},
+{
+    "message": "{\"timestamp\":\"2019-11-13 17:08:33\",\"service\":\"inter-chatbot-dti-brokers-service\",\"thread\":\"[qtp1878169648-17733]\",\"level\":\"INFO\",\"meta\":\"ID_CONVERSA: 24526524935 - MENSAGEM: Esta Habilitado para conversar - TIMESTAMP: 2019-11-13 17:08:33.241\"}"
+},
+{
+    "message": "{\"timestamp\":\"2019-11-13 17:08:33\",\"service\":\"inter-chatbot-dti-brokers-service\",\"thread\":\"[qtp1878169648-17733]\",\"level\":\"INFO\",\"meta\":\"ID_CONVERSA: 24526524935 - MENSAGEM: finalizandoFlow: VerificaChatBotHabilitadoFlowItem - TIMESTAMP: 2019-11-13 17:08:33.241\"}"
+},
+{
+    "message": "{\"timestamp\":\"2019-11-13 17:08:33\",\"service\":\"inter-chatbot-dti-brokers-service\",\"thread\":\"[qtp1878169648-17733]\",\"level\":\"INFO\",\"meta\":\"ID_CONVERSA: 24526524935 - MENSAGEM: iniciandoFlow: ConverterNotificacaoParaMensagemFlowItem - TIMESTAMP: 2019-11-13 17:08:33.241\"}"
+}
+]
+
+const start = () => {
+  let mensagens = [];
+
+  LOGS_COLAR_AQUI.map(e => {
+    const message = JSON.parse(e.message);
+    const newObjMessage = {
+      date: new Date(message.timestamp),
+      messageLog: message.meta
+    };
+    mensagens.push(newObjMessage);
+  });
+
+  mensagens = mensagens.sort((a, b)=> a.date.getTime() - b.date.getTime());
+  mensagens.map(msg => {
+    console.log("-----------------------------------------")
+    console.log(`TIME ==> `, msg.date)
+    console.log(`LOG ==> `, msg.messageLog)
+  })
+};
+
+start();
